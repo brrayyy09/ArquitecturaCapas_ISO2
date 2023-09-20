606870ee5825dd2674e06c7da4de13d8caa29522
brrayyy09 <brayanmaca94@gmail.com>
2023-09-20 17:31:46 -0500

    agregue controlador para getFiltersHandler

---
+++ src/controllers/filters/getFIlters.mjs
@@ -0,0 +1,18 @@
+import Process from '../../models/Process.mjs';
+
+const getFilters = async (id) => {
+    try {
+        const process = await Process.findById(id);
+
+        if (!process || !process.files || process.files.length === 0) {
+            return { message: 'Imagen no encontrada' };
+        }
+        const filters = process.filters; // Accede al campo filters del objeto process
+        return { message: `Imagen encontrada, Los filtros aplicados para esta imagen son: ${filters}` };
+    } catch (error) {
+        throw Boom.badData(error.message, { error });
+    }
+}
+
+
+export default getFilters;
--- src/handlers/filters/getFiltersHandler.mjs
+++ src/handlers/filters/getFiltersHandler.mjs
@@ -1,25 +1,17 @@
-import mongoose from 'mongoose';
-import Process from '../../models/Process.mjs';
-import { StatusCodes } from 'http-status-codes';
+import getFilters from '../../controllers/filters/getFIlters.mjs';
+import Boom from "@hapi/boom";
+import HttpStatusCodes from "http-status-codes";
 
-const getFiltersHandler = (async (req, res, next) => {
+const getFiltersHandler = async (req, res, next) => {
     try {
         const { id } = req.params;
         console.log(id);
-        const process = await Process.findById(id);
-
-        if (!process || !process.files || process.files.length === 0) {
-            return res.sendStatus(400).json({ message: 'Imagen no encontrada' });
-        }
-
-        const filters = process.filters; // Accede al campo filters del objeto process
-        return res.send(`Imagen encontrada, Los filtros aplicados para esta imagen son: ${filters}`);
-
+        const response = await getFilters(id);
+        return res.status(HttpStatusCodes.OK).json(response);
     } catch (error) {
-        console.error(error);
-        res.sendStatus(500).json({ message: 'Error al obtener la imagen' });
+        const err = Boom.isBoom(error) ? error : Boom.internal(error);
+        next(err);
     }
-    next();
-});
+};
 
 export default getFiltersHandler;
