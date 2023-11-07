539a80ad74f4de3553d2f4a39fa31bc63bdb4bf4
{HANA0K4} <{mateohanaoka@gmail.com}>
2023-11-07 14:11:27 -0500

    Se agrego imageFilterService

--- src/container/buildContainer.mjs
+++ src/container/buildContainer.mjs
@@ -1,13 +1,19 @@
 import ProcessRepository from '../repositories/ProcessRepository.mjs';
 import MinioService from '../services/MinioService.mjs';
 import ProcessService from '../services/ProcessService.mjs';
+import ImageFilterService from '../services/ImageFilterService.mjs';
 
 const buildContainer = (req, res, next) => {
   const container = {};
 
   const processRepository = new ProcessRepository();
   const minioService = new MinioService();
-  const processService = new ProcessService({ processRepository, minioService });
+  const imageFilterService = new ImageFilterService();
+  const processService = new ProcessService({
+    processRepository,
+    minioService,
+    imageFilterService,
+  });
 
   container.processService = processService;
 
