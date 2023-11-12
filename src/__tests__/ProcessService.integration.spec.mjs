ed119a7888dc5385b05cc2921adaf66a7ebbbafa
jhojan16 <jhojan.espinosa@uao.edu.co>
2023-11-12 12:09:27 -0500

    más pruebas

--- src/__tests__/ProcessService.integration.spec.mjs
+++ src/__tests__/ProcessService.integration.spec.mjs
@@ -25,6 +25,52 @@ describe('ProcessService', () => {
     it('should throw an error if the payload does not contain a valid files array', async () => {
       await expect(processService.applyFilters({ files: ['invalid', 'file'] })).rejects.toThrow();
     });
+
+
+
+    it('should apply a blur filter to an image', async () => {
+
+        const imageBuffer = Buffer.from('image data');
+        const filters = [BLUR_FILTER];
+    
+        const imageWithFilter = await processService.imageFilterService.applyFilters(imageBuffer, filters);
+    
+ 
+        expect(imageWithFilter).not.toBe(imageBuffer);
+  
+      });
+    
+      it('should save an image to Minio', async () => {
+        const imageBuffer = Buffer.from('image data');
+        const fileName = 'image.png';
+    
+        const imageUrl = await processService.minioService.saveImage({
+          buffer: imageBuffer,
+          originalname: fileName,
+        });
+    
+        expect(imageUrl).not.toBeNull();
+      });
+  
+      it('should save a process to the database', async () => {
+        const filters = [BLUR_FILTER];
+        const images = [{ imageUrl: 'https://example.com/image.png', filters: [] }];
+    
+        const process = await processService.processRepository.save({ filters, images });
+    
+    
+        expect(process).not.toBeNull();
+      });
+    
+      
+      it('should get a process by its ID', async () => {
+        const processId = 1;
+    
+        const process = await processService.getProcessById(processId);
+    
+       
+        expect(process).not.toBeNull();
+      });
   
     
 });
